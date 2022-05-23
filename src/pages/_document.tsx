4102f621337215c8beff5767ccfb19d31b509bf2
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-05-23 00:17:40 -0300

    fix: vercel deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import Document from 'next/document'
   import { ServerStyleSheet } from 'styled-components'
   
   export default class MyDocument extends Document {
+  //here
     static async getInitialProps(ctx): any {
       const sheet = new ServerStyleSheet()
       const originalRenderPage = ctx.renderPage
